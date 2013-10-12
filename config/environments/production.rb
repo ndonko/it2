1984f9f552e8e79b3336526200d97af6b85358ea
ndonko <panytanguy6@yahoo.fr>
2013-10-12 20:45:54 +0200

    mwc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ It2Solution::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-   config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!

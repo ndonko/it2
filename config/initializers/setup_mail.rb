cc12e20e0a7a4c6effe694c5d01e358e27903b5b
ndonko <panytanguy6@yahoo.fr>
2013-10-12 19:52:02 +0200

    mc1

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-require 'development_mail_interceptor'
+#require 'development_mail_interceptor'
 #ActionMailer::Base.delivery_method = :smtp
 #ActionMailer::Base.perform_deliveries = true
 #ActionMailer::Base.raise_delivery_errors = true
@@ -13,22 +13,8 @@ require 'development_mail_interceptor'
  #  :enable_starttls_auto   => true
  #}
  
-  #gmail configuration for heroku 
-config.action_mailer.default_url_options = { :host => 'it2.herokuapp.com' }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "it2.herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-}
+  
   
  
 # ActionMailer::Base.default_url_options[:host] = "localhost:3000"
- Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+ #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
